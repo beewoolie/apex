6856c1942851aa7e54fad0bb67205dc3b42f1535
elf <elf>
2004-10-31 08:01:03 +0000

    Some cleanup

--- src/arch-arm/.entry.o.cmd
+++
@@ -1,9 +0,0 @@
-cmd_src/arch-arm/entry.o := /usr/arm-linux/gcc-3.3-glibc-2.3.2/bin/arm-linux-gcc -Wp,-MD,src/arch-arm/.entry.o.d -nostdinc -iwithprefix include  -I/m/elf/home/z/embedded/apex/src/arch-arm -Isrc/arch-arm     -c -o src/arch-arm/entry.o src/arch-arm/entry.c
-
-deps_src/arch-arm/entry.o := \
-  src/arch-arm/entry.c \
-    $(wildcard include/config/h.h) \
-
-src/arch-arm/entry.o: $(deps_src/arch-arm/entry.o)
-
-$(deps_src/arch-arm/entry.o):
