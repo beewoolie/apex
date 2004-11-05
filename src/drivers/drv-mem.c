077f2a924a3f376baac984164a4c4ec060cef2c0
elf <elf>
2004-11-05 21:07:29 +0000

    copy and erase functions

--- src/drivers/drv-mem.c
+++ src/drivers/drv-mem.c
@@ -25,15 +25,118 @@ struct mem_descriptor {
   size_t ib;
 };
 
+struct mem_region {
+  void* pv;
+  size_t cb;
+};
+
+static struct mem_region regions[32];
 static struct mem_descriptor descriptors[2];
+
+#define CB_BLOCK	     (1024*1024)
+
+
+void get_memory_map(void)
+{
+  	u32* pl;
+	int i;
+
+	/* Clear all memory region identifiers */
+	for(i = 0; i < NUM_MEM_AREAS; i++)
+		memory_map[i].used = 0;
+
+	/* Mark the blocks in reverse order */
+	for (pl = (u32*) (MEMORY_END - CB_BLOCK);
+	     pl >= (u32*) MEMORY_START;
+	     pl -= CB_BLOCK/sizeof (*pl)) {
+	  *pl = (u32) pl;
+	}
+
+	/* Identify contiguous, unaliases blocks  */
+	i = 0;
+	for (pl = (u32*) MEMORY_START;
+	     pl != (u32*) MEMORY_END && i < NUM_MEM_AREAS;
+	     pl += CB_BLOCK/sizeof (*pl)) {
+		if (testram ((u32) pl) != 0)
+			continue;
+		if (*pl == (u32) pl) {
+		  	if (memory_map[i].used == 0) {
+				memory_map[i].start = (u32) pl;
+				memory_map[i].used = 1;
+				memory_map[i].len = CB_BLOCK;
+			}
+			else
+			  memory_map[i].len += CB_BLOCK;
+		}
+		else
+			if (memory_map[i].used)
+				++i;
+	}
+
+	/* Show the memory map */
+	SerialOutputString("Memory map:\n");
+	for(i = 0; i < NUM_MEM_AREAS; i++) {
+		if(memory_map[i].used) {
+#if defined (DO_CLEAR_MEM)
+		  	clear_mem (memory_map[i].start, memory_map[i].len);
+#endif
+			SerialOutputString("  @ 0x");
+			SerialOutputHex(memory_map[i].start);
+			SerialOutputString(" 0x");
+			SerialOutputHex(memory_map[i].len);
+			SerialOutputString(" (");
+			SerialOutputDec(memory_map[i].len / (1024 * 1024));
+			SerialOutputString(" MiB)\n");
+		}
+	}
+}
+
 
 static int memory_probe (void)
 {
-  /* Need to probe memory here so that we can pass information to
-     kernel.  Also, it should be used to bound open() calls.   */
+  extern char APEX_VMA_START;
+  extern char APEX_END;
+  unsigned long* pl;
+  int i;
 
-  printf ("probing 0x%08x # 0x%08x\r\n", 
-	  CONFIG_MEM_BANK0_START, CONFIG_MEM_BANK0_LENGTH);
+  memset (&regions, 0, sizeof (regions));
+
+	/* Mark */
+  for (pl = (unsigned long*) (CONFIG_MEM_BANK0_START
+			      + CONFIG_MEM_BANK0_LENGTH
+			      - CB_BLOCK
+			      + (&APEX_END - &APEX_VMA_START));
+       pl >= (unsigned long*) CONFIG_MEM_BANK0_START;
+       pl -= CB_BLOCK/sizeof (*pl))
+    *pl = (unsigned long) pl;
+
+	/* Identify */
+  i = 0;
+  for (pl = (unsigned long*) (CONFIG_MEM_BANK0_START
+			      + (&APEX_END - &APEX_VMA_START));
+       pl < (unsigned long*) (CONFIG_MEM_BANK0_START + CONFIG_MEM_BANK0_LENGTH)
+	 && i < sizeof (regions)/sizeof (struct mem_region);
+       pl += CB_BLOCK/sizeof (*pl)) {
+    //    if (testram ((u32) pl) != 0)
+    //      continue;
+    if (*pl == (unsigned long) pl) {
+      if (region[i].cb == 0) {
+	region[i].pv = (void*) pl - (&APEX_END - &APEX_VMA_START);
+	region[i].cb = CB_BLOCK;
+      }
+      else
+	region[i].cb += CB_BLOCK;
+    }
+    else
+      if (region[i].cb)
+	++i;
+  }
+
+  printf ("\r\nmemory\r\n");
+
+  for (i = 0; i < sizeof (regions)/sizeof (struct mem_region); ++i) {
+    printf (" 0x%08p 0x%08x (%d MiB)\r\n", 
+	    regions[i].pv, regions[i].cb, regions[i].cb/(1024*1024));
 
   return 0;			/* Present and initialized */
 }
