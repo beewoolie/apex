11bfd74b5722c6e47d41c68095e7ad5a3c7d59bc
elf <elf>
2004-11-11 00:06:49 +0000

    Copyrights are in

--- src/apex/console.c
+++ src/apex/console.c
@@ -4,7 +4,7 @@
    written by Marc Singer
    1 Nov 2004
 
-   Copyright (C) 2004 The Buici Company
+   Copyright (C) 2004 Marc Singer
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License as
