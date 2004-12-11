fc96863bd017b2b5e2e00e14f34cf881c8934abc
elf <elf>
2004-12-11 07:09:05 +0000

    Checkpoint on audio

--- src/mach-lh7952x/codec.c
+++ src/mach-lh7952x/codec.c
@@ -31,6 +31,11 @@
 
 #define USE_I2S
 //#define USE_CPU_MASTER
+#define USE_DMA
+//#define USE_DMA_CAP
+#define USE_16
+//#define USE_STEREO
+#define USE_LOOPBACK
 
 #define T_SKH	1		/* Clock time high (us) */
 #define T_SKL	1		/* Clock time low (us) */
@@ -38,16 +43,17 @@
 #define T_CSS	1		/* Minimum chip select setup time (us)  */
 #define T_DIS	1		/* Data setup time (us) */
 
-#define SSP_CTRL0	(0x00)
-#define SSP_CTRL1	(0x04)
-#define SSP_DR		(0x08)
-#define SSP_SR		(0x0c)
-#define SSP_CPSR	(0x10)
-#define SSP_IMSC	(0x14)
-#define SSP_RIS		(0x18)
-#define SSP_MIS		(0x1c)
-#define SSP_ICR		(0x20)
-#define SSP_DCR		(0x24)
+#define SSP_CTRL0	__REG(SSP_PHYS + 0x00)
+#define SSP_CTRL1	__REG(SSP_PHYS + 0x04)
+#define SSP_DR_PHYS	(SSP_PHYS + 0x08)
+#define SSP_DR		__REG(SSP_PHYS + 0x08)
+#define SSP_SR		__REG(SSP_PHYS + 0x0c)
+#define SSP_CPSR	__REG(SSP_PHYS + 0x10)
+#define SSP_IMSC	__REG(SSP_PHYS + 0x14)
+#define SSP_RIS		__REG(SSP_PHYS + 0x18)
+#define SSP_MIS		__REG(SSP_PHYS + 0x1c)
+#define SSP_ICR		__REG(SSP_PHYS + 0x20)
+#define SSP_DCR		__REG(SSP_PHYS + 0x24)
 
 #define SSP_SR_BSY	(1<<4)
 #define SSP_SR_REFI	(1<<3)
@@ -55,12 +61,12 @@
 #define SSP_SR_TNF	(1<<1)	/* Transmit FIFO not full */
 #define SSP_SR_TFE	(1<<0)	/* Transmit FIFO empty */
 
-#define I2S_CTRL	(0x00)
-#define I2S_STAT	(0x04)
-#define I2S_IMSC	(0x08)
-#define I2S_RIS		(0x0c)
-#define I2S_MIS		(0x10)
-#define I2S_ICR		(0x14)
+#define I2S_CTRL	__REG(SSP_PHYS + 0x00)
+#define I2S_STAT	__REG(SSP_PHYS + 0x04)
+#define I2S_IMSC	__REG(SSP_PHYS + 0x08)
+#define I2S_RIS		__REG(SSP_PHYS + 0x0c)
+#define I2S_MIS		__REG(SSP_PHYS + 0x10)
+#define I2S_ICR		__REG(SSP_PHYS + 0x14)
 
 #define I2S_CTRL_MCLKINV (1<<4)
 #define I2S_CTRL_WSDEL	(1<<3)
@@ -142,6 +148,7 @@ static void execute_spi_command (int v, int cwrite)
   //  unsigned long l = 0;
   unsigned char reg;
 
+  printf ("spi 0x%04x -> 0x%x\r\n", v & 0x1ff, (v >> CODEC_ADDR_SHIFT) & 0x7f);
   enable_cs ();
 
   reg = __REG8 (CPLD_SPI) & ~CPLD_SPI_TX;
@@ -184,25 +191,26 @@ static void execute_spi_command (int v, int cwrite)
 
 static void codec_unmute (void)
 {
-  execute_spi_command (CMD (CODEC_POWER_CTRL, 
-			    (1<<2) /* Disable ADC  */
+  execute_spi_command (CMD (CODEC_POWER_CTRL, 0
+			    | (1<<2) /* Disable ADC  */
 			    | (1<<1) /* Disable MIC  */
 			    | (1<<0) /* Disable LINE IN */
 			    ), 16);
-  execute_spi_command (CMD (CODEC_ANALOG_CTRL, 
-			    (1<<4) /* Enable DAC */
-			    |(1<<1) /* Mute Microphone */
+  execute_spi_command (CMD (CODEC_ANALOG_CTRL, 0
+			    | (1<<4) /* Enable DAC */
+			    | (1<<1) /* Mute Microphone */
 			    ), 16);
   execute_spi_command (CMD (CODEC_DIGITAL_ACTIVATE, (1<<0)), 16);
 }
 
 static void codec_mute (void)
 {
-  execute_spi_command (CMD (CODEC_ANALOG_CTRL, 
-			    (0<<4) /* Disable DAC */
-			    |(1<<1) /* Mute Microphone */
+  execute_spi_command (CMD (CODEC_ANALOG_CTRL, 0
+			    | (0<<4) /* Disable DAC */
+			    | (1<<1) /* Mute Microphone */
 			    ), 16);
-  execute_spi_command (CMD (CODEC_DIGITAL_ACTIVATE, (0<<0)), 16);
+  execute_spi_command (CMD (CODEC_DIGITAL_ACTIVATE, 
+			    (0<<0)), 16);
 }
 
 static void codec_configure (int frequency, int sample_size)
@@ -245,62 +253,193 @@ static void codec_configure (int frequency, int sample_size)
   }
 
   execute_spi_command (CMD (CODEC_SAMPLE_RATE, v), 16);
-  //  usleep (1);
 
-  __REG (SSP_PHYS + SSP_CTRL0) 
-    = (1<<4)			/* TI synchronous mode */
-    | ((sample_size - 1) & 0xf)<<0;
+  //  MASK_AND_SET (SSP_CTRL0, 0xf, (sample_size - 1) & 0xf);
+  //  SSP_CTRL0 
+  //    = (1<<4)			/* TI synchronous mode */
+  //    | ((sample_size - 1) & 0xf)<<0;
 }
 
 static void codec_init (void)
 {
+  SSP_CTRL0 = 0;
+  SSP_CTRL1 = 0;
+
   __REG (RCPC_PHYS + RCPC_CTRL) |= RCPC_CTRL_UNLOCK;
   MASK_AND_SET (__REG (RCPC_PHYS + RCPC_CTRL),  /* System osc. -> CLKOUT */
 		3<<5, 0<<5);
   __REG (RCPC_PHYS + RCPC_PCLKSEL1) |= 1<<1;	/* System osc. -> SSP clock */
+//  __REG (RCPC_PHYS + RCPC_PCLKSEL1) &= ~(1<<1);	/* HCLK -> SSP clock */
 #if defined (USE_CPU_MASTER)
-  __REG (RCPC_PHYS + RCPC_SSPPRE) = (8>>1);	/* Prescalar = 8 */
+//  __REG (RCPC_PHYS + RCPC_SSPPRE) = (8>>1);	/* Prescalar = 8 */
+  __REG (RCPC_PHYS + RCPC_SSPPRE) = (1>>1);	/* Prescalar = 1 */
 #else
   __REG (RCPC_PHYS + RCPC_SSPPRE) = (1>>1);	/* Prescalar = 1 */
 #endif
   __REG (RCPC_PHYS + RCPC_PCLKCTRL1) &= ~(1<<1); /* Enable SSP clock */
   __REG (RCPC_PHYS + RCPC_CTRL) &= ~RCPC_CTRL_UNLOCK;
 
+#if defined (USE_CPU_MASTER)
+  SSP_CPSR = 16;
+#else
+  SSP_CPSR = 2; /* Smallest prescalar is 2 */
+#endif
+
+  SSP_CTRL1 |= (1<<1); /* Enable */
+
+		/* Wait for TX/RX FIFOs to empty */
+  while ((SSP_SR & SSP_SR_TFE) == 0)
+    ;
+  while (SSP_SR & SSP_SR_RNE)
+    SSP_DR;
+
+  SSP_ICR = 0xff;		/* Clear interrupts */
+  SSP_CTRL1 &= ~(1<<1);		/* Disable */
+
+  SSP_IMSC &= 0xff;		/* Mask everything */
+
   MASK_AND_SET (__REG (IOCON_PHYS + IOCON_MUXCTL5),
 		(3<<6)|(3<<4)|(3<<2)|(3<<0),
 		(1<<6)|(1<<4)|(1<<2)|(1<<0));	/* SSP/I2S signals */
 //  __REG (IOCON_PHYS + IOCON_RESCTL5) &= ~((3<<6)|(3<<4)|(3<<2)|(3<<0));
 
-  __REG (SSP_PHYS + SSP_CTRL1) = 0
-#if defined (USE_CPU_MASTER)
-    | (0<<2)			/* master */
-#else
-    | (1<<2)			/* slave */
+  /* *** this is where Jun Li's code leave the I2S hardware */
+
+#if 0
+  /* Master mode */
+
+    /* Set SCLK polarity as normal polarity */
+    ssp_ioctl(dev_ssp, SSP_SET_SCLK_POLARITY, 0);
+    
+    /* Set SCLK phase as normal */
+    ssp_ioctl(dev_ssp, SSP_SET_SCLK_PHASE, 0);
+    
+    /* Set SSP working in loopback mode */
+    ssp_ioctl(dev_ssp, SSP_LOOP_BACK_ENABLE, 1);
+    
+    /* Enable SSP */
+    ssp_ioctl(dev_ssp, SSP_ENABLE, 1);
+    
+    /* Set SSP frame format */
+    ssp_ioctl(dev_ssp, SSP_SET_FRAME_FORMAT, SSP_MODE_TI);
+    
+    /* Set SSP data size */
+    ssp_ioctl(dev_ssp, SSP_SET_DATA_SIZE, 16);
+    
+    /* Set SSP speed in us */
+    ssp_ioctl(dev_ssp, SSP_SET_SPEED, freq*16);
+    
+    /* Init SSP DMA mode */
+    ssp_ioctl(dev_ssp, SSP_DMA_TX_RX_INIT, 0);
 #endif
-    | (1<<1)			/* enabled */
-    | (0<<3);			/* SSP can driver SSPTX */
-#if defined (USE_CPU_MASTER)
-  __REG (SSP_PHYS + SSP_CPSR) = 16;
-#else
-  __REG (SSP_PHYS + SSP_CPSR) = 2; /* Smallest prescalar is 2 */
+
+#if 0
+    /* Set SSP frame format */
+    ssp_ioctl(dev_ssp, SSP_SET_FRAME_FORMAT, SSP_MODE_TI);
+    
+    /* Set SSP in slave mode */
+    ssp_ioctl(dev_ssp, SSP_SET_SLAVE, 1);
+    
+     /* Set SSP data size */
+    ssp_ioctl(dev_ssp, SSP_SET_DATA_SIZE, 16);
+    
+    /* Init SSP DMA mode */
+    ssp_ioctl(dev_ssp, SSP_DMA_TX_INIT, 0);
+        
+    /* Set source address */
+    ssp_ioctl(dev_ssp, SSP_DMA_SET_SOURCE, (INT_32)source);
+
+    /* Set count */
+    ssp_ioctl(dev_ssp, SSP_DMA_SET_COUNT, count);
+            
+     /* Enable TX FIFO int */
+    //ssp_ioctl(dev_ssp, SSP_INT_ENABLE, SSP_TRANSMIT_FIFO_INT);
+            
+    ssp_data_source = source;
+    
+    ssp_data_left = count;
+    
+    ssp_data_current = 0;
+
 #endif
+
+
+    MASK_AND_SET (SSP_CTRL0, (3<<4), (1<<4)); /* TI mode */
+#if defined (USE_CPU_MASTER)
+    SSP_CTRL1 &= ~(1<<2);	/* Master mode */
+#else
+    SSP_CTRL1 |=  (1<<2);	/* Slave mode */
+#endif
+
+#if defined (USE_LOOPBACK)
+    SSP_CTRL1 |= (1<<0);	/* Loopback */
+#endif
+
+#if defined (USE_16)
+    MASK_AND_SET (SSP_CTRL0, 0xf, ((16 - 1) & 0xf));
+#else
+    MASK_AND_SET (SSP_CTRL0, 0xf, ((8  - 1) & 0xf));
+#endif    
+
+#if defined (USE_DMA)
+  __REG (RCPC_PHYS + RCPC_CTRL) |= RCPC_CTRL_UNLOCK;
+  __REG (RCPC_PHYS | RCPC_AHBCLKCTRL) &= ~(1<<0); /* Enable DMA AHB clock */
+  __REG (RCPC_PHYS + RCPC_CTRL) &= ~RCPC_CTRL_UNLOCK;
+
+  DMA0_CTRL
+    = (0<<13)			/* Peripheral source */
+    | (0<<9)			/* Load base addresses on start  */
+    | (1<<7)			/* Destination size 2 bytes */
+    | (1<<3)			/* Source size is 2 bytes */
+    | (0<<2)			/* Destination fixed */
+    | (0<<1);			/* Source fixed */
+
+  DMA1_CTRL
+    = (1<<13)			/* Peripheral destination */
+#if defined (USE_16)
+    | (1<<7)			/* Destination size 2 bytes */
+    | (1<<3)			/* Source size is 2 bytes */
+//    | (1<<5)			/* Source burst 4 incrementing */
+#else
+    | (0<<7)			/* Destination size 1 bytes */
+    | (0<<3)			/* Source size is 1 byte */
+//    | (1<<5)			/* Source burst 4 incrementing */
+#endif
+    | (0<<9)			/* Wrapping: Load base addresses on start  */
+    | (0<<2)			/* Destination fixed */
+    | (1<<1);			/* Source incremented */
+
+  DMA_CLR   = 0xff;
+//  DMA_MASK |= (1<<1);		/* Enable of DMA channel 1 */
+#endif
+  
+  //  SSP_IMSC = (1<<3);		/* Allow TX interrupts */
+
+#if defined (USE_DMA)
+  SSP_DCR = 0
+    | (1<<1)			/* TX DMA enabled */
+    | (1<<0)			/* RX DMA enabled */
+    ;
+#else
+  SSP_DCR = 0;
+#endif
+
 #if defined (USE_I2S)
   __REG (I2S_PHYS + I2S_CTRL)
     = I2S_CTRL_I2SEN
+#if defined (USE_LOOPBACK)
+    | I2S_CTRL_I2SLBM
+#endif
     ;
 #endif
 
-  codec_mute ();
+//  codec_mute ();
 
   execute_spi_command (CMD (CODEC_RESET, 0), 16);
-  //  usleep (1);
-  //  usleep (1);
   execute_spi_command (CMD (CODEC_DIGITAL_CTRL, 
 			    0
 //			    |(2<<1) /* 44.1 kHz deemphasis */
 			    ), 16);
-  //  usleep (1);
-  //  usleep (1);
   execute_spi_command (CMD (CODEC_DIGITAL_FORMAT,
 			    0
 #if defined (USE_CPU_MASTER)
@@ -311,7 +450,7 @@ static void codec_init (void)
 //			    | (1<<4) /* LRP: right channel on LRC low */
 			    | (0<<2) /* 16 bit */
 //			    | (3<<2) /* 32 bit */
-#if defined (USE_I2S)
+#if defined (USE_I2S) && 0
 			    | (2<<0) /* I2S format */
 #else
 			    | (3<<0) /* DSP format */
@@ -333,21 +472,17 @@ static void codec_release (void)
   __REG (RCPC_PHYS + RCPC_CTRL) &= ~RCPC_CTRL_UNLOCK;
 }
 
-#define USE_DMA
-#define USE_16
 
 static int cmd_codec_test (int argc, const char** argv)
 {
-  static unsigned short __attribute__((section("pcm.data"))) buffer[16384];
-  int i;
-  int cSampleMax = sizeof (rgbPCM);
-  static unsigned long cap;
-
 #if defined (USE_16)
-#define rgb buffer  
+  static unsigned short __attribute__((section("pcm.data"))) buffer[65000];
 #else
-#define rgb rgbPCM
+  static unsigned char  __attribute__((section("pcm.data"))) buffer[65000];
 #endif
+  int i;
+  int cSampleMax = sizeof (buffer)/sizeof (buffer[0]);
+  static unsigned long cap;
 
 #if defined (USE_16)
   codec_configure (FREQUENCY_8K, 16);
@@ -357,128 +492,107 @@ static int cmd_codec_test (int argc, const char** argv)
 
   codec_unmute ();
 
-#if defined (USE_16)
-  cSampleMax = sizeof (buffer)/sizeof (buffer[0]);
+#if defined (USE_STEREO)
+  cSampleMax /= 2;
+#endif
+
+  if (cSampleMax > sizeof (rgbPCM))
+    cSampleMax = sizeof (rgbPCM);
+
   for (i = 0; i < cSampleMax; ++i) {
-#if defined (USE_CPU_MASTER)
+#if defined (USE_16)
     unsigned short v = (rgbPCM[i]<<8) | rgbPCM[i];
-    v = v/2;// + 0x8000/2;
-    buffer[i++] = v;		/* Go stereo */
-    buffer[i] = v;
 #else
-    unsigned short v = (rgbPCM[i]<<8) | rgbPCM[i];
-    v = v/2;// + 0x8000/2;
+    unsigned char  v = rgbPCM[i];
+#endif
+    v = v/2; // + 0x8000/2; /* soften */
+#if defined (USE_STEREO)
+    buffer[i*2] = v;
+    buffer[i*2 + 1] = v;
+#else
     buffer[i] = v;
 #endif
   }
+
+#if defined (USE_STEREO)
+  cSampleMax *= 2;
 #endif
 
 #if defined (USE_DMA)
-  __REG (RCPC_PHYS + RCPC_CTRL) |= RCPC_CTRL_UNLOCK;
-  __REG (RCPC_PHYS | RCPC_AHBCLKCTRL) &= ~(1<<0); /* Enable DMA AHB clock */
-  __REG (RCPC_PHYS + RCPC_CTRL) &= ~RCPC_CTRL_UNLOCK;
+//  __REG (RCPC_PHYS + RCPC_CTRL) |= RCPC_CTRL_UNLOCK;
+//  __REG (RCPC_PHYS | RCPC_AHBCLKCTRL) &= ~(1<<0); /* Enable DMA AHB clock */
+//  __REG (RCPC_PHYS + RCPC_CTRL) &= ~RCPC_CTRL_UNLOCK;
 
-  //  __REG (SSP_PHYS + SSP_IMSC) |= 0
+  //  SSP_IMSC) |= 0
   //    | (1<<3)
   //    | (1<<2);			/* Interrupts needed for DMA */
-  __REG (SSP_PHYS + SSP_DCR)  |= 0
-    | (1<<1)			/* TX DMA enabled */
-    | (1<<0)			/* RX DMA enabled */
-    ;
+//  SSP_DCR  |= 0
+//    | (1<<1)			/* TX DMA enabled */
+//    | (1<<0)			/* RX DMA enabled */
+//    ;
 
-  __REG (DMA_PHYS + DMA_MASK)	   = 0; /* Mask all interrupts */
-  __REG (DMA_PHYS + DMA_CLR)	   = 0xff; /* Clear pending interrupts */
+//  DMA_MASK	= 0; /* Mask all interrupts */
+//  DMA_CLR	= 0xff; /* Clear pending interrupts */
 
-#if 1
-  __REG (DMA0_PHYS + DMA_CTRL)     = 0; /* Disable */
-  __REG (DMA0_PHYS + DMA_SOURCELO) =  (SSP_PHYS + SSP_DR)          & 0xffff;
-  __REG (DMA0_PHYS + DMA_SOURCEHI) = ((SSP_PHYS + SSP_DR) >> 16)   & 0xffff;
-  __REG (DMA0_PHYS + DMA_DESTLO)   =  ((unsigned long)&cap)        & 0xffff;
-  __REG (DMA0_PHYS + DMA_DESTHI)   = (((unsigned long)&cap) >> 16) & 0xffff;
-  __REG (DMA0_PHYS + DMA_MAX)	   = 0xffff;
-  __REG (DMA0_PHYS + DMA_CTRL)
-    = (0<<13)			/* Peripheral source */
-    | (0<<9)			/* Load base addresses on start  */
-    | (1<<7)			/* Destination size 2 bytes */
-    | (1<<3)			/* Source size is 2 bytes */
-    | (0<<2)			/* Destination fixed */
-    | (0<<1);			/* Source fixed */
+//  DMA0_CTRL     = 0; /* Disable */
+  DMA0_SOURCELO =  SSP_DR_PHYS        & 0xffff;
+  DMA0_SOURCEHI = (SSP_DR_PHYS >> 16) & 0xffff;
+  DMA0_DESTLO   =  ((unsigned long)&cap)        & 0xffff;
+  DMA0_DESTHI   = (((unsigned long)&cap) >> 16) & 0xffff;
+  DMA0_MAX	= 0xffff;
+//  DMA0_CTRL
+//    = (0<<13)			/* Peripheral source */
+//    | (0<<9)			/* Load base addresses on start  */
+//    | (1<<7)			/* Destination size 2 bytes */
+//    | (1<<3)			/* Source size is 2 bytes */
+//    | (0<<2)			/* Destination fixed */
+//    | (0<<1);			/* Source fixed */
 
-#endif
-
-  __REG (DMA1_PHYS + DMA_CTRL)     = 0; /* Disable */
-  __REG (DMA1_PHYS + DMA_SOURCELO) =  ((unsigned long)rgb)        & 0xffff;
-  __REG (DMA1_PHYS + DMA_SOURCEHI) = (((unsigned long)rgb) >> 16) & 0xffff;
-  __REG (DMA1_PHYS + DMA_DESTLO)   =  (SSP_PHYS + SSP_DR)	     & 0xffff;
-  __REG (DMA1_PHYS + DMA_DESTHI)   = ((SSP_PHYS + SSP_DR) >> 16)     & 0xffff;
-  __REG (DMA1_PHYS + DMA_MAX)	   = cSampleMax;
-  __REG (DMA1_PHYS + DMA_CTRL)
-    = (1<<13)			/* Peripheral destination */
-#if defined (USE_16)
-    | (1<<7)			/* Destination size 2 bytes */
-    | (1<<3)			/* Source size is 2 bytes */
+//  DMA1_CTRL     = 0; /* Disable */
+  DMA1_SOURCELO =  ((unsigned long)buffer)        & 0xffff;
+  DMA1_SOURCEHI = (((unsigned long)buffer) >> 16) & 0xffff;
+  DMA1_DESTLO   =  SSP_DR_PHYS        & 0xffff;
+  DMA1_DESTHI   = (SSP_DR_PHYS >> 16) & 0xffff;
+  DMA1_MAX	= cSampleMax;
+//  DMA1_CTRL
+//    = (1<<13)			/* Peripheral destination */
+//#if defined (USE_16)
+//    | (1<<7)			/* Destination size 2 bytes */
+//    | (1<<3)			/* Source size is 2 bytes */
+////    | (1<<5)			/* Source burst 4 incrementing */
+//#else
+//    | (0<<7)			/* Destination size 1 bytes */
+//    | (0<<3)			/* Source size is 1 byte */
 //    | (1<<5)			/* Source burst 4 incrementing */
-#else
-    | (0<<7)			/* Destination size 1 bytes */
-    | (0<<3)			/* Source size is 1 byte */
-    | (1<<5)			/* Source burst 4 incrementing */
-#endif
-    | (0<<9)			/* Wrapping: Load base addresses on start  */
-    | (0<<2)			/* Destination fixed */
-    | (1<<1);			/* Source incremented */
+//#endif
+//    | (0<<9)			/* Wrapping: Load base addresses on start  */
+//    | (0<<2)			/* Destination fixed */
+//    | (1<<1);			/* Source incremented */
 
-  __REG (DMA0_PHYS + DMA_CTRL) |= (1<<0); /* Enable RX DMA */
-  __REG (DMA1_PHYS + DMA_CTRL) |= (1<<0); /* Enable TX DMA */
+//  return 0;			/* *** FIXME */
+
+  SSP_CTRL1 |= (1<<1);		/* Enable SSP  */
+#if defined (USE_DMA_CAP)
+  DMA0_CTRL |= (1<<0);		/* Enable RX DMA */
+#endif
+  DMA1_CTRL |= (1<<0);		/* Enable TX DMA */
 
 				/* Wait for completion */
-  while ((__REG (DMA_PHYS + DMA_STATUS) & (1<<1)) == 0)
+  while ((DMA_STATUS & (1<<1)) == 0)
     ;
 
 #else
 
 #if defined (USE_16)
 
+  SSP_CTRL1 |= (1<<1); /* Enable SSP */
+
   for (i = 0; i < cSampleMax; ++i) {
-    //    unsigned char v = rgbPCM[i]/2 + 0x8000/2;
-
 				/* Wait for room in the FIFO */
-    while ((__REG16 (SSP_PHYS + SSP_SR) & SSP_SR_TNF) == 0)
+    while (!(SSP_SR & SSP_SR_TNF))
       ;
-    //    __REG (SSP_PHYS + SSP_DR) = ((rgbPCM[i] << 8)|rgbPCM[i]);
-//    __REG16 (SSP_PHYS + SSP_DR) = (v<<8)|(v);
-    __REG16 (SSP_PHYS + SSP_DR) = rgb[i];
-				/* Wait for room in the FIFO */
-    //    while ((__REG (SSP_PHYS + SSP_SR) & SSP_SR_TNF) == 0)
-    //      ;
-    //    __REG (SSP_PHYS + SSP_DR) = (rgbPCM[i] << 8)|rgbPCM[i];
+    SSP_DR = buffer[i];
   }
-#else
-
-  for (i = 0; i < sizeof (rgbPCM); ++i) {
-//    unsigned char v = rgbPCM[i]/2 + 0x8000/2;
-    unsigned char v = rgbPCM[i];
-    
-				/* Wait for room in the FIFO */
-    while ((__REG16 (SSP_PHYS + SSP_SR) & SSP_SR_TNF) == 0)
-      ;
-    //    __REG (SSP_PHYS + SSP_DR) = ((rgbPCM[i] << 8)|rgbPCM[i]);
-//    __REG16 (SSP_PHYS + SSP_DR) = (v<<8)|(v);
-    __REG16 (SSP_PHYS + SSP_DR) = v;
-				/* Wait for room in the FIFO */
-    //    while ((__REG (SSP_PHYS + SSP_SR) & SSP_SR_TNF) == 0)
-    //      ;
-    //    __REG (SSP_PHYS + SSP_DR) = (rgbPCM[i] << 8)|rgbPCM[i];
-  }
-
-				/* Clear the pipe  */
-  __REG (SSP_PHYS + SSP_DR) = 0x7fff;
-  __REG (SSP_PHYS + SSP_DR) = 0x7fff;
-  __REG (SSP_PHYS + SSP_DR) = 0x7fff;
-  __REG (SSP_PHYS + SSP_DR) = 0x7fff;
-  __REG (SSP_PHYS + SSP_DR) = 0x7fff;
-  __REG (SSP_PHYS + SSP_DR) = 0x7fff;
-  __REG (SSP_PHYS + SSP_DR) = 0x7fff;
-  __REG (SSP_PHYS + SSP_DR) = 0x7fff;
 #endif
 
 #endif /* !USE_DMA */
