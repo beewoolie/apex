8c1413abec42a8236b6d65a53972d69c11afca40
elf <elf>
2005-03-08 05:42:46 +0000

    Checkpoint with some new patches

--- src/mach-ixp42x/_intel/env.sh
+++ src/mach-ixp42x/_intel/env.sh
@@ -4,4 +4,4 @@ export CROSS_COMPILE=/usr/armv5b-softfloat-linux/gcc-3.4.2-glibc-2.3.3/bin/armv5
 export LINUX_CROSS_COMPILE=$CROSS_COMPILE
 export IX_TARGET=linuxbe
 export IX_XSCALE_SW=`pwd`/ixp400_xscale_sw
-export linuxbe_KERNEL_DIR=`pwd`/linux-2.6.11
+export linuxbe_KERNEL_DIR=`pwd`/linux
