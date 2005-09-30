c5a46b4f4688ae0d4bcf061d9ce6ef69211a4cae
elf <elf>
2005-09-30 19:13:39 +0000

    Unified SDRAM boot report

---
+++ include/sdramboot.h
@@ -0,0 +1,34 @@
+/* sdramboot.h
+     $Id$
+
+   written by Marc Singer
+   30 Sep 2005
+
+   Copyright (C) 2005 Marc Singer
+
+   -----------
+   DESCRIPTION
+   -----------
+
+*/
+
+#if !defined (__SDRAMBOOT_H__)
+#    define   __SDRAMBOOT_H__
+
+/* ----- Includes */
+
+#include <attributes.h>
+
+/* ----- Types */
+
+/* ----- Globals */
+
+#if defined (CONFIG_SDRAMBOOT_REPORT)
+extern int __xbss(init) fSDRAMBoot;
+#endif
+
+/* ----- Prototypes */
+
+
+
+#endif  /* __SDRAMBOOT_H__ */
