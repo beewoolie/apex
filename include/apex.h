58860e349d7cf32d778344465572c6888aaf6f16
Marc Singer <elf@buici.com>
2011-06-17 15:04:56 -0700

    Building all configurations with GCC 4.5.2.
    o Added a couple of new functions to the arch-arm/lib that GCC requires.
    o Changed all overrides to use the same compiler.
    o Fixed prototype for dumpw.

--- include/apex.h
+++ include/apex.h
@@ -71,7 +71,7 @@ static inline void msleep (int c) {	/* Only way to guarantee the range */
 
 
 #define dump(p,c,i) dumpw(p,c,i,0)
-extern void dumpw (const char* rgb, int cb,
+extern void dumpw (const void* rgb, size_t cb,
 		   unsigned long index, int width);
 
 #if defined (CONFIG_COMMAND_HISTORY)
